d9a8d3d438cd80216877b179be05104088845685
Haru Sakai <harusakai0515@gmail.com>
2024-07-28 17:58:36 -0500

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,20 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Haru Sakai',
-  description: 'My portfolio page',
-}
+  title: "Haru Sakai",
+  description: "My portfolio page",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
